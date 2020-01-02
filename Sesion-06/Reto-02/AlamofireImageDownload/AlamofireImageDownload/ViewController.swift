0cde6f49676a433b83d848f2e2f4160251716970
Ricardo Montesinos <richimf1@gmail.com>
2020-01-02 16:41:24 +0100

    Sesion-06

--- Sesion-06/Reto-02/AlamofireImageDownload/AlamofireImageDownload/ViewController.swift
+++ Sesion-06/Reto-02/AlamofireImageDownload/AlamofireImageDownload/ViewController.swift
@@ -10,7 +10,12 @@ import Alamofire
 
 class ViewController: UIViewController {
   
+  
   @IBOutlet weak var imageView: UIImageView!
+  
+  var download: DownloadRequest?
+  var resumeData: Data!
+  
   let url = "https://ucarecdn.com/6db0d613-ea78-4417-9aa8-22075bb08f99/-/scale_crop/320x210/center"
   
   override func viewDidLoad() {
@@ -20,11 +25,30 @@ class ViewController: UIViewController {
   }
   
   func downloadImage() {
-    AF.download(url).responseData { response in
+    self.download = AF.download(url).responseData { response in
+      if let data = response.value {
+        self.imageView.image = UIImage(data: data)
+      }
+    }
+    .downloadProgress { progress in
+      let prog = 100*progress.fractionCompleted
+      print("Download Progress: \(prog)")
+    }
+  }
+  
+  func pause() {
+    self.download?.cancel { data in
+      self.resumeData = data
+    }
+  }
+  
+  func resumeDownload() {
+    AF.download(resumingWith: resumeData).responseData { response in
       if let data = response.value {
         self.imageView.image = UIImage(data: data)
       }
     }
   }
+  
 }
 
