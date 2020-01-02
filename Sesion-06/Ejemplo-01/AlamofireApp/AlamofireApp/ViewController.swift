a8e1a08e2584683da477aadd8967169f9b250625
Ricardo Montesinos <richimf1@gmail.com>
2020-01-02 15:31:47 +0100

    Sesion-06, Ejemplo-01

---
+++ Sesion-06/Ejemplo-01/AlamofireApp/AlamofireApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlamofireApp
+//
+//  Created by Ricardo Montesinos on 02/01/20.
+//  Copyright © 2020 Bedu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
